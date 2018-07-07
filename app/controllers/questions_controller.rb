c0c6d78bbbdb18709d508d888f0647406d5a5e83
Janusz Kojro <januszkojro@MacBook-Pro-Janusz.local>
2018-07-07 14:52:39 +0200

    add question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,25 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+  	@question = Question.new
+  end
+
+  def create
+  	@question = current_user.questions.build(question_params)
+  	@question.save
+
+  	 redirect_to question_path(@question)
+  end
+
+  def show
+  	@question = Question.find(params[:id])
+  end
+
+  private
+
+  def question_params
+  	params.require(:question).permit(:title, :content)
+  end
 end
