22198bfb989253f9ac96cf349c93609d4531a394
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-07 14:25:23 +0200

    Profile view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'user/show'
   devise_for :users
+  resources :users, only: [:show]
   root to: 'home#index'
   resources :home, only: :index
   resources :questions, only: :index
